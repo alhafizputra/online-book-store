2f4bb906539f91506689edc389a6f004d936d691
Alhafiz Putra <alhafiz_putra@telkomsel.co.id>
2021-02-09 18:50:41 +0700

    add bean validation

--- src/main/java/com/glints/onlinebookstore/payload/BookCategoryPayload.java
+++ src/main/java/com/glints/onlinebookstore/payload/BookCategoryPayload.java
@@ -1,9 +1,13 @@
 package com.glints.onlinebookstore.payload;
 
+import javax.validation.constraints.NotBlank;
+
 public class BookCategoryPayload {
 	
+	@NotBlank(message = "name is mandatory")
 	private String name;
 
+	@NotBlank(message = "code is mandatory")
 	private String code;
 
 	public String getName() {
